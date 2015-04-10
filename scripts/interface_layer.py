ab631e7e605409eff9d6c36b07403970c5bdf7ff
Bianca Homberg <bhomberg@mit.edu>
2015-04-10 16:34:40 -0400

    added references to state and world throughout

--- scripts/interface_layer.py
+++ scripts/interface_layer.py
@@ -6,30 +6,32 @@ from task_motion_planner.srv import *
 from task_motion_planner.msg import *
 
 MAX_TRAJ_COUNT = 999
+task_server = None
 # TODO: if necessary, add in random seed for pose generators later
 
-def run_interface_layer(state, initialPose, task_server):
+def run_interface_layer(state, world, initialPose):
     initial_state = state
+    initial_world = world
     step = None
     hlplan = None
     partialTraj = None
     pose1 = None
     sub = tryRefineClass()
     if hlplan == None:
-        hlplan = callTaskPlanner(state, task_server)
+        hlplan = callTaskPlanner(state, world)
         step = 1
         partialTraj = None
         pose1 = initialPose
     while(resource limit not reached):
-        (success, refinement) = sub.try_refine(pose1, hlplan, step, partialTraj, mode='errorFree')
+        (success, refinement) = sub.try_refine(pose1, state, world, hlplan, step, partialTraj, mode='errorFree')
         if success:
             return refinement
         success = False
         trajCount = 0
         while not success and trajCount < MAX_TRAJ_COUNT:
-            (partialTraj, pose2, failStep, failCause) = sub.try_refine(pose1, hlplan, step, partialTraj, mode='partialTraj')
-            state = stateUpdate(state, failCause, failStep)
-            (success, newPlan) = callTaskPlanner(state, task_server)
+            (partialTraj, pose2, failStep, failCause, state, world) = sub.try_refine(pose1, state, world hlplan, step, partialTraj, mode='partialTraj')
+            (state, world) = stateUpdate(state, world, failCause, failStep)
+            (success, newPlan) = callTaskPlanner(state, world)
             if success:
                 hlplan = hlplan[0:failStep] + newPlan
                 pose1 = pose2
@@ -42,10 +44,10 @@ def run_interface_layer(state, initialPose, task_server):
         pose1 = initialPose
 
 
-def callTaskPlanner(state, task_server):
+def callTaskPlanner(state, world):
     # plan is an array of tuples, let's say, where the first thing is the action and the rest are objects it acts on
     msg = task_domain()
-    # output state to file
+    #TODO: output state and world to file
     msg.task_file = 'state'
     resp = task_server(msg)
     # parse plan file into appropriate action tuple
@@ -55,19 +57,21 @@ def callTaskPlanner(state, task_server):
         plan.append(tuple(line.rsplit(' ')))
     return (resp.plan.error, plan)
 
-def stateUpdate(state, failCause, failStep):
+def stateUpdate(state, world, failCause, failStep):
     return state
 
-def get_motion_plan(pose1, pose2):
+def get_motion_plan(world, state, axn, goalPose):
     pass
 
-def MPErrs(pose1, pose2, state):
+def MPErrs(pose1, pose2, state, world):
     pass
 
 
 class tryRefineClass:
 
     def __init__(self):
+        self.world = None
+        self.state = None
         self.index = None
         self.traj = None
         self.called = False
@@ -77,11 +81,13 @@ class tryRefineClass:
         self.nextaxn = None
         self.poseGen = PoseGenerator()
 
-    def try_refine(initialPose, hlplan, step, trajprefix, mode='errorFree'):
+    def try_refine(initialPose, state, world, hlplan, step, trajprefix, mode='errorFree'):
         if self.called == False or self.old_hlplan != hlplan:
             self.index = step - 1
+            self.state = state
             self.traj = trajprefix
             self.pose1 = initialPose
+            self.called = True
         while step-1 <= self.index and self.index <= len(hlplan):
             self.axn = hlplan[index]
             self.nextaxn = hlplan[index+1]
@@ -92,7 +98,7 @@ class tryRefineClass:
                 index--
                 self.traj = traj.delSuffixFor(self.axn) # TODO: define this (trajectory object?)
             else:
-                (succeeds, motionPlan) = get_motion_plan(self.pose1, self.pose2)
+                (succeeds, state, motionPlan) = get_motion_plan(self.pose1, self.pose2)
                 if succeeds:
                     if self.index == len(hlplan)+1:
                         return traj
@@ -100,12 +106,12 @@ class tryRefineClass:
                     self.index++
                     self.pose1 = self.pose2
                 else if mode == 'partialTraj':
-                    return (self.pose1, self.traj, self.index+1, MPErrs(self.pose1, self.pose2, state))
+                    return (self.pose1, self.traj, self.index+1, MPErrs(self.pose1, self.pose2, self.state, self.world), self.state, self.world)
 
 if __name__ == "__main__":
     rospy.wait_for_service('task_server_service')
     task_server = rospy.ServiceProxy('task_server_service', task_service)
-    state = None
-    initialPose = None
+    state = None # TODO: acquire state message and put in proper format
+    initialPose = None # TODO: acquire initial pose and put in proper format 
     run_interface_layer(state, initialPose, task_server)
  
