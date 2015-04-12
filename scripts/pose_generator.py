500a33f28eb93f871badfda3da30f463e8fc4029
Veronica Lane <vmlane@mit.edu>
2015-04-11 20:20:49 -0400

    pose generator takes in actions & world as params

--- scripts/pose_generator.py
+++ scripts/pose_generator.py
@@ -7,33 +7,50 @@ from task_motion_planner.msg import *
 import math
 import random
 import copy
+import re
 
 class PoseGenerator:
-	# cylinder dimensions
-	def __init__(self):
-		self.table_height = .05
-	
-	def pickup(self,x,y,z,length,radius):
+	def __init__(self, table_height=0.25):
+		self.table_height = table_height
+
+	def generate(self, action, world):
+		action = re.split(',', action[1:-1])
+		obj_name = action[-1]
+		objects = world.world.collision_objects
+		obj_idx = self._search_for_object(obj_name, objects)
+		obj = objects[obj_idx]
+		height = obj.primitives[0].dimensions[0]
+		radius = obj.primitives[0].dimensions[1]
+		position = obj.primitive_poses[0].position
+
+		if action[0] == 'pickup':
+			self.pickup(position,height,radius)
+		elif action[0] == 'putdown':
+			pass
+		else:
+			return
+
+	def pickup(self, obj_pose, height, radius):
 		# gripper distance from cylinder centerpoint during staging
 		r = radius + .03 
 		# height of gripper when grabbing
-		z = self.table_height + .03
+		z =  obj_pose.position.z + height/2
 
 		# stage, generate a random pose on a larger circle around the cylinder
 		poseGen1 = pose_gen()
 		pose1 = poseGen1.pose
-		pose1.position.y = random.uniform(y-r,y+r)
-		pose1.position.x = math.sqrt(r**2 - (pose1.position.y - y)**2) - x
+		pose1.position.y = random.uniform(obj_pose.position.y-r, obj_pose.position.y+r)
+		pose1.position.x = math.sqrt(r**2 - (pose1.position.y - obj_pose.position.y)**2) - obj_pose.position.x
 		pose1.position.z = z
-		yaw = math.atan2(pose1.position.y/pose1.position.x)
-		pose1.orientation = rpy2Orientation(0,0,yaw)
+		yaw = math.atan2(pose1.position.y,pose1.position.x)
+		pose1.orientation = self._rpy_to_orientation(0,0,yaw)
 		poseGen1.gripperOpen = True
 
 		#pre-grasp
 		poseGen2 = pose_gen()
 		pose2 = poseGen2.pose
-		pose2.position.x = radius*(pose1.position.x/math.sqrt(pose1.position.x**2+pose1.position.y**2)) + x
-		pose2.position.y = radius*(pose1.position.y/math.sqrt(pose1.position.x**2+pose1.position.y**2)) + y
+		pose2.position.x = radius*(pose1.position.x/math.sqrt(pose1.position.x**2+pose1.position.y**2)) + obj_pose.position.x
+		pose2.position.y = radius*(pose1.position.y/math.sqrt(pose1.position.x**2+pose1.position.y**2)) + obj_pose.position.y
 		pose2.position.z = z
 		pose2.orientation = pose1.orientation
 		poseGen2.gripperOpen = True
@@ -52,15 +69,15 @@ class PoseGenerator:
 		pose4 = poseGen4.pose
 		pose4.position.x = pose2.position.x
 		pose4.position.y = pose2.position.y
-		pose4.position.z = table_height + .15
+		pose4.position.z = self.table_height + .15
 		pose4.orientation = pose3.orientation
 		poseGen4.gripperOpen = False
 		
 		# move out of the way to a standard position
 		poseGen5 = pose_gen()
 		pose5 = poseGen5.pose
-		pose5.position.z = table_height + .15
-		pose5.orientation = rpy2Orientation(0,0,0)
+		pose5.position.z = self.table_height + .15
+		pose5.orientation = self._rpy_to_orientation(0,0,0)
 		poseGen5.gripperOpen = False
 
 		#Return array of custom pose messages - pose + boolean (open/closed)
@@ -94,15 +111,15 @@ class PoseGenerator:
 		poseGen4.pose.position.z = poseGen3.pose.position.z
 		poseGen4.pose.orientation = poseGen3.pose.orientation
 		poseGen4.gripperOpen = True
-	    
-	   	# move up
+		
+		# move up
 		poseGen5 = pose_gen()
 		poseGen5.pose.position.x = poseGen4.pose.position.x
 		poseGen5.pose.position.y = poseGen4.pose.position.y
 		poseGen5.pose.position.z = poseGen5.pose.position.z + .10
 		poseGen5.pose.orientation = poseGen4.pose.orientation
 		poseGen5.gripperOpen = True
-	        
+			
 		# move out of the way
 		poseGen6 = pose_gen()
 		poseGen6.pose.position.z = self.table_height + .10
@@ -111,7 +128,13 @@ class PoseGenerator:
 		# Return array of custom pose messages - pose + boolean (open/closed)
 		return [poseGen1,poseGen2,poseGen3,poseGen4,poseGen5,poseGen6]
 
-	def rpy2Orientation(roll,pitch,yaw):
+	def _search_for_object(self, obj_name, obj_list):
+		for i in range(len(obj_list)):
+			if obj_name == obj_list[i].id:
+				return i                
+		return -1
+
+	def _rpy_to_orientation(self, roll, pitch, yaw):
 		c1 = math.cos(yaw/2)
 		if pitch == math.pi/2:
 			c2 = math.cos(math.pi/4)
@@ -134,5 +157,9 @@ class PoseGenerator:
 		return result
 
 if __name__ == "__main__":
-    poseGen = PoseGenerator()
-    print poseGen.putdown(0,0,1,1)
+	poseGen = PoseGenerator()
+	pose = Pose()
+	pose.position.x = .1
+	pose.position.y = .1
+	pose.position.z = .27
+	print poseGen.pickup(pose,.05,.02)
