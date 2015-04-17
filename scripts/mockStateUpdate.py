96df89233401badb66fcbbcb9f45aba173a76d7c
Bianca Homberg <bhomberg@mit.edu>
2015-04-16 21:22:15 -0400

    removed excess print statements

--- scripts/mockStateUpdate.py
+++ scripts/mockStateUpdate.py
@@ -5,33 +5,14 @@ import rospy
 from task_motion_planner.msg import *
 import copy
 
-# TODO: change world to message type not dictionary
-
 def mockStateUpdate(state_input, failCause, failStep, prev_fail_step, hlplan, world):
         # stateUpdate will need to be updated based on the specific problem
-        # currently set up for mocks
         state = copy.deepcopy(state_input)
-        print "prev fail step: ", prev_fail_step
-        print "curr: ", failStep
         for i in range(failStep-prev_fail_step):
-            print "blah: ", i+prev_fail_step+1
             action = hlplan[i+prev_fail_step+1]
-            print action
-            #print action
-            #print state[1]
             if action[0] == 'PICKUP':
                 state[1].remove( ('EMPTY', action[2]) )
                 state[1].append( ('NOT', 'EMPTY', action[2]) )
-                for obj in world.world.movable_objects:
-                    t = ('ROBOTAT', 'GP_' + obj.id)
-                    if t in state[1]:
-                        print t
-                        print action[3]
-                    for loc in world.world.surfaces:
-                        t = ('ROBOTAT', 'PDP_' + obj.id + '_' +  loc.id)
-                        if t in state[1]:
-                            print t
-                            print action[3]
                 state[1].remove( ('ROBOTAT', action[3]) )
                 state[1].append( ('ROBOTAT', action[4]) )
                 state[1].append( ('NOT', 'ROBOTAT', action[3]) )
@@ -41,7 +22,6 @@ def mockStateUpdate(state_input, failCause, failStep, prev_fail_step, hlplan, wo
                     t = ('OBSTRUCTS', action[1], obj.id)
                     if t in state[1]:
                         state[1].remove(t)
-
                     for loc in world.world.surfaces:
                             state[1].append( ('NOT', 'PDOBSTRUCTS', action[1], obj.id, loc.id) )
                             t = ('PDOBSTRUCTS', action[1], obj.id, loc.id)
@@ -69,13 +49,9 @@ def mockStateUpdate(state_input, failCause, failStep, prev_fail_step, hlplan, wo
             obj_to_pickup = action[1]
             for obj in failCause:
                 state[1].append( ('OBSTRUCTS', obj, obj_to_pickup))
-                #print "ADDED STATE (pickup): ", state[1][-1]
-                #print state[1]
         elif action[0] == 'PUTDOWN':
             obj_to_putdown = action[1]
             tloc = action[5]
             for obj in failCause:
                 state[1].append( ('PDOBSTRUCTS', obj, obj_to_putdown, tloc))
-                #print "ADDED STATE (putdown): ", state[1][-1]
-                #print state[1]
         return state
